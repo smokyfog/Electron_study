191471bb59f33e18291aaa9821e086eb9f24d4ad
smokyfog <1473092739@qq.com>
2020-01-14 00:08:43 +0800

    七牛工具方法等

---
+++ cloud-doc/test.js
@@ -0,0 +1,35 @@
+const qiniu = require('qiniu')
+const QiniuManager = require('./src/utils/QiniuManager')
+
+
+var accessKey = 'hoFpxX2IdHXjDc7jVSDRYrMPJsV0xk1QqH288R6J'
+var secretKey = 'ZW4RDTOITtv4G1dqoF5lRcXzUx4ZN3XeUEGNteZ4'
+var localFile = "/Users/yanqiang/Documents/md/test1.md"
+var key='test1.md'
+
+const manager = new QiniuManager(accessKey, secretKey, 'electron-notes')
+// manager.uploadFile(key, localFile).then(data => {
+//   console.log('上传成功', data)
+//   return manager.deleteFile(key)
+// }).then(data => {
+//   console.log('删除成功', data)
+// })
+// manager.getBuckteDomain().then(data => {
+//   console.log(data)
+// })
+manager.generateDownloadLink(key).then(data => {
+  console.log(data)
+  return manager.generateDownloadLink('first.md')
+}).then(data => {
+  console.log(data)
+})
+// manager.deleteFile(key)
+
+
+
+// var bucketManager = new qiniu.rs.BucketManager(mac, config);
+
+// // 公开空间访问链接
+// var publicDownloadUrl = bucketManager.publicDownloadUrl(publicBucketDomain, key);
+// console.log(publicDownloadUrl);
+
